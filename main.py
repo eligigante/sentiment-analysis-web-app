63f65f0ccb37bf6ad71c075bdc395dfe03a36c30
eligigante <elijahg975@gmail.com>
2024-06-29 01:39:30 +0800

    modify layout

--- main.py
+++ main.py
@@ -3,21 +3,46 @@ import pandas as pd
 import streamlit as st
 import cleantext
 
-st.header('Reddit Sentiment Analyzer')
+def get_sentiment_emoji(sentiment):
+    if sentiment == 'Positive':
+        return '😃'
+    elif sentiment == 'Negative':
+        return '😞'
+    else:
+        return '😐'
+
+col1, col2 = st.columns([1, 9])
+
+with col1:
+    st.write("")
+    st.image('images/reddit_logo.png', width=65)  # Adjust width as needed
+
+with col2:
+    st.title('Reddit Sentiment Analyzer')
+
 st.text("A simple web application for analyzing reddit sentiments. Receive immediate feedback \non the sentiment, whether it is positive or negative")
 
 with st.expander('Analyze Text'):
     text = st.text_input('Input text here: ')
     if text:
         blob = TextBlob(text)
-        st.write('Polarity: ', round(blob.sentiment.polarity,2))
-        st.write('Subjectivity: ', round(blob.sentiment.subjectivity,2))
+        polarity = round(blob.sentiment.polarity, 2)
+        subjectivity = round(blob.sentiment.subjectivity, 2)
+        st.write('Polarity: ', polarity)
+        st.write('Subjectivity: ', subjectivity)
+
+        if polarity >= 0.5:
+            st.write('Sentiment: Positive', get_sentiment_emoji('Positive'))
+        elif polarity <= -0.5:
+            st.write('Sentiment: Negative', get_sentiment_emoji('Negative'))
+        else:
+            st.write('Sentiment: Neutral', get_sentiment_emoji('Neutral'))
 
     pre = st.text_input('Clean Text: ')
     if pre:
-        st.write(cleantext.clean(pre, clean_all= False, extra_spaces=True ,
-                                 stopwords=True ,lowercase=True ,numbers=True , punct=True))
-        
+        st.write(cleantext.clean(pre, clean_all=False, extra_spaces=True,
+                                 stopwords=True, lowercase=True, numbers=True, punct=True))
+
 with st.expander('Analyze CSV'):
     upl = st.file_uploader('Upload file')
 
@@ -33,7 +58,6 @@ with st.expander('Analyze CSV'):
         else:
             return 'Neutral'
 
-#
     if upl:
         df = pd.read_excel(upl)
         del df['Unnamed: 0']
